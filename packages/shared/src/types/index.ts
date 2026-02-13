18a5f74f7c33926645e4a3401ec0bbe7f72478ab
Mansoor <mansoorahamedsaaho@gmail.com>
2026-02-13 17:00:09 +0530

    Add types barrel export file

---
+++ packages/shared/src/types/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Types Barrel Export
+ * 
+ * Central export point for all type definitions.
+ * Import types from this file in other packages and applications.
+ */
+
+// User types
+export * from './user.types';
+
+// Organization and team types
+export * from './organization.types';
+
+// Project and task types
+export * from './project.types';
+
+// Time tracking types
+export * from './time-tracking.types';
+
+// Notification types
+export * from './notification.types';
+
+// API and common types
+export * from './api.types';
