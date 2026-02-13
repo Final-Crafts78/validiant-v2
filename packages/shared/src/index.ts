27eb7e3d06d8bef540a135c249dcc599678167aa
Mansoor <mansoorahamedsaaho@gmail.com>
2026-02-13 17:09:35 +0530

    Add main shared package export file

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Validiant Shared Package
+ * 
+ * Shared types, constants, and utilities used across all applications.
+ * This is the main entry point for the @validiant/shared package.
+ * 
+ * @packageDocumentation
+ */
+
+// Export all types
+export * from './types';
+
+// Export all constants
+export * from './constants';
+
+// Export all utilities
+export * from './utils';
